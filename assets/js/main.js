faa408aa73ef1d62bbfd3a541ac1ebcb5d921c05
bozkurtozdemir95 <bozkurtozdemir95@gmail.com>
2020-11-01 09:41:47 +0400

    header first row

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,26 @@ $(document).ready(function () {
         $(e.target).parents().find('.custom-select-menu').toggleClass('open');
     });
 });
+$(function () {
+    $('.datetimepicker.date').datetimepicker({
+        language: 'en',
+        pickDate: true,
+        pickTime: false,
+        pickSeconds: false,
+        maskInput: true,
+        format: 'dd/MM/yyyy',
+    });
+    $('.datetimepicker.time').datetimepicker({
+        language: 'en',
+        pickDate: false,
+        pickTime: true,
+        pickSeconds: false,
+        maskInput: true,
+        format: 'hh:mm',
+    });
+});
+
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
