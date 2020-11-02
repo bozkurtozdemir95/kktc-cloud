88209286a3c483c23c43dcdfd3e5fb893885dbf4
bozkurtozdemir95 <bozkurtozdemir95@gmail.com>
2020-11-02 11:23:16 +0400

    css fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,7 +58,7 @@ $(document).ready(function(){
         margin:10,
         nav:true,
         navText: ["<svg class='icon'><use href='../assets/icons/sprite.svg#left-arrow'></use></svg> <div class='line'></div>"," <div class='line'></div><svg class='icon'><use href='../assets/icons/sprite.svg#right-arrow'></use></svg>"],
-        autoplay: false,
+        autoplay: true,
         autoplayTimeout: 3000,
         responsive:{
             0:{
