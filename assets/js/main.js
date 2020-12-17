88ffa1e00b30546ee41f8276fd23f134dee49ab0
bozkurtozdemir95 <bozkurtozdemir95@gmail.com>
2020-12-17 20:35:06 +0400

    news home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -389,6 +389,46 @@ $(function() {
     autoplay: true,
     autoplaySpeed: 3000,
   });
+  $('.news-slider').slick({
+    dots: false,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    draggable: true,
+    swipeToSlide: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 
   $('.slider-for').slick({
     slidesToShow: 1,
